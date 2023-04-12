59182d83a02a91ef2d958bf69e0fce68c5bdfd1f
Paula Campos <pkcamposc@falabella.cl>
2023-04-12 17:46:41 -0400

    refactor: Crear reducer simple

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,13 +1,11 @@
-import { Action } from "@ngrx/store";
-import { decrementar, incrementar } from "./contador.actions";
+import { createReducer, on } from '@ngrx/store';
+import * as actions from './contador.actions';
 
-export function contadorReducer(state: number = 10, action: Action) {
-    switch(action.type) {
-        case incrementar.type:
-            return state + 1;
-        case decrementar.type:
-            return state - 1;
-        default:
-            return state;
-    }
-}
+export const initialState = 20;
+
+export const contadorReducer = createReducer(
+  initialState,
+  on(actions.incrementar, (state) => state + 1),
+  on(actions.decrementar, (state) => state - 1),
+  on(actions.reset, (state) => 0)
+);
